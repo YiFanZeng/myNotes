0a9eebd46f0bf3620414fcc22a41c2b0797638e6
zengyifan <yfxf_china@163.com>
2016-08-07 04:12:52 -0400

    wsgi: hello world

---
+++ wsgi/server.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+from wsgiref.simple_server import make_server
+from helloworld import application
+
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000 ...')
+httpd.serve_forever()
