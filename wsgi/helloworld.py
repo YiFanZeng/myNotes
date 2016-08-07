7a6a775e1e2e19deb942ea3741846274514b3ea6
zengyifan <yfxf_china@163.com>
2016-08-07 04:18:44 -0400

    test: git pull faulure
    Signed-off-by: zengyifan <yfxf_china@163.com>

--- wsgi/helloworld.py
+++ wsgi/helloworld.py
@@ -4,4 +4,5 @@ def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
     body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
     # return ["Hello World"]
+    # return [body.encode('utf-8')]
     return [body.encode('utf-8')]
