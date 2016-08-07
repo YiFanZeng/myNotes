a74d7b0f72312ecdc37461bfb1d7be8b1ca2a263
zengyifan <yfxf_china@163.com>
2016-08-06 22:18:02 -0400

    wsgi: init dir and test
    Signed-off-by: zengyifan <yfxf_china@163.com>

---
+++ wsgi/simplewsgi.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from paste import httpserver
+
+def application(environ, start_response):
+    start_response('200 OK', [('Context-type', 'text/html')])
+    return ['Hello World\n']
+
+httpserver.serve(application, host='127.0.0.1', port=8080)
